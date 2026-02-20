aa57cfe2e82d377ae06097553143e5204ef335be
Brian Souza <brian-desouza@hotmail.com>
2026-02-20 17:48:28 -0300

    ajustando pastas

--- src/agentes/agente_po.py
+++ src/agentes/agente_po.py
@@ -2,7 +2,7 @@ from agentes.agente_base import AgenteBase
 
 class AgentePO(AgenteBase):
     def __init__(self):
-        super().__init__("prompts/refina_storyprompt.txt")
+        super().__init__("src/prompts/refina_storyprompt.txt")
     
     async def run(self, story: str) -> str:
         resposta = await self.agente.run(story)
